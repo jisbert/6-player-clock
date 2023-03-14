8a8836bc22e449eca8638c79be3ac81c8503de65
Jose Gisbert <jisbert@users.noreply.github.com>
2023-03-14 20:03:22 +0100

    Compile from VS Code include and src

---
+++ include/buzzer.hpp
@@ -0,0 +1,6 @@
+class Buzzer {
+    const int buzzer_pin;
+public:
+    Buzzer(int new_buzzer_pin);
+    void beep();
+};
---
+++ src/buzzer.cpp
@@ -0,0 +1,7 @@
+#include "include/buzzer.hpp"
+
+Buzzer::Buzzer(int new_buzzer_pin): buzzer_pin(new_buzzer_pin) {}
+
+void Buzzer::beep() {
+
+}
