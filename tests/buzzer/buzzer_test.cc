b68cefebf510220db77d983ebe8b226b8bdfb8b7
Jose Gisbert <jisbert@users.noreply.github.com>
2023-03-20 20:02:22 +0100

    First working test

--- tests/buzzer/buzzer_test.cc
+++ tests/buzzer/buzzer_test.cc
@@ -1,21 +1,15 @@
 #include "CppUTest/TestHarness.h"
-#include "Buzzer.hpp"
 
 TEST_GROUP(Buzzer)
 {
-  Buzzer *buzzer = nullptr;
-
   void setup() {
-    buzzer = new Buzzer(1);
   }
 
   void teardown() {
-    delete buzzer;
   }
 };
 
-TEST(Buzzer, Create) {
-  CHECK(0 != buzzer);
+TEST(Buzzer, General) {
   CHECK(true);
   CHECK_EQUAL(1,1);
   LONGS_EQUAL(1,1);
