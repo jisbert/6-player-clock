6c78c7c48c0c054b31530494656d563062dbe2e3
Jose Gisbert <jisbert@users.noreply.github.com>
2023-04-11 19:44:26 +0200

    Improve MinGW64 code coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,18 @@
-cd build
+#!/bin/bash
+
+# Bash script to create code coverage report on Windows MinGW64
+# This cannot be accomplished using CodeCoverage cmake plugin
+# cause a failure while running lcov
+#
+# Usage
+# -----
+#
+# $ cmake -B coverage -DENABLE_TESTS=ON
+# $ cmake --build tests --target tests
+# $ chmod +x coverage.sh
+# $ ./coverage.sh
+
+cd coverage
 lcov -d . -b tests -z
 tests/tests.exe
 lcov -d . -b tests -c -o coverage.info
