bafa0efe4fe0b714b38e6c33b9288f5888b88f7c
Jose Gisbert <jisbert@users.noreply.github.com>
2023-04-13 18:20:57 +0200

    Clean app source

--- src/app.cc
+++ src/app.cc
@@ -2,14 +2,11 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-#include <stdio.h>
 
 #include "pico/stdlib.h"
 
-#include "buzzer/buzzer.h"
-
 int main() {
   stdio_init_all();
-  Buzzer buzzer(1);
+
   return 0;
 }
