8c20b43b1db1ed87106c91d48ea536c13ba50892
Jose Gisbert <jisbert@users.noreply.github.com>
2023-04-13 18:19:41 +0200

    Fix cpplint script

--- cpplint.sh
+++ cpplint.sh
@@ -9,4 +9,4 @@
 # $ chmod +x cpplint.sh
 # $ ./cpplint.sh
 
-cpplint --counting=detailed ./include/button/button_controller.h ./include/button/button_event.h ./include/button/button_handler.h ./include/buzzer/buzzer.h ./src/app.cc ./src/button/button_controller.cc ./src/button/button_event.cc ./src/button/button_handler.cc ./src/buzzer/buzzer.cc ./tests/mocks/include/button/button_handler_mock.h ./tests/mocks/src/button/button_handler_mock.cc ./tests/src/button/button_controller_tests.cc ./tests/src/button/button_handler_tests.cc ./tests/src/buzzer/buzzer_tests.cc ./tests/src/tests_app.cc
+cpplint --counting=detailed $( find . -name \*.h -or -name \*.cc | grep -vE "^\.\/(build|coverage)\/" )
