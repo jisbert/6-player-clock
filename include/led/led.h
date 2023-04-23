e1986547fd40eef91074da8eb461a338b896d8ba
Jose Gisbert <jisbert@users.noreply.github.com>
2023-04-23 19:03:04 +0200

    Remove base directory header

---
+++ include/led/led.h
@@ -0,0 +1,16 @@
+/* Copyright (c) 2023 Jose Gisbert
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef LED_LED_H_
+#define LED_LED_H_
+
+constexpr std::uint16_t kPlayer1LedPin = 11;
+constexpr std::uint16_t kPlayer2LedPin = 12;
+constexpr std::uint16_t kPlayer3LedPin = 13;
+constexpr std::uint16_t kPlayer4LedPin = 14;
+constexpr std::uint16_t kPlayer5LedPin = 15;
+constexpr std::uint16_t kPlayer6LedPin = 16;
+
+#endif  // LED_LED_H_
