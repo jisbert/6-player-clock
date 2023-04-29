b0085171e882deba688eda08e5793f154ea40b83
Jose Gisbert <jisbert@users.noreply.github.com>
2023-04-29 17:22:35 +0200

    Clean buzzer test suite

--- tests/src/buzzer/buzzer_tests.cc
+++ tests/src/buzzer/buzzer_tests.cc
@@ -7,24 +7,6 @@
 
 #include "CppUTest/TestHarness.h"
 
-TEST_GROUP(Buzzer) {
-  Buzzer* buzzer;
+TEST_GROUP(Buzzer) {};
 
-  void setup() {
-    buzzer = new Buzzer(1);
-  }
-
-  void teardown() {
-    delete buzzer;
-  }
-};
-
-TEST(Buzzer, General) {
-  CHECK(0 != buzzer);
-  CHECK(true);
-  CHECK_EQUAL(1, 1);
-  LONGS_EQUAL(10, 10);
-  DOUBLES_EQUAL(1.000, 1.001, .01);
-  STRCMP_EQUAL("hello", "hello");
-  // FAIL("The prior tests pass, but this one doesn't");
-}
+TEST(Buzzer, Beep) {}
