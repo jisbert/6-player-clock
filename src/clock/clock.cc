6f5c7ebe68cc8e7895566b9902c9d250493aeb41
Jose Gisbert <jisbert@users.noreply.github.com>
2023-05-01 18:56:25 +0200

    Fix whitespace linter error

--- src/clock/clock.cc
+++ src/clock/clock.cc
@@ -28,7 +28,11 @@ bool Clock::Resume(std::uint16_t remaining_seconds) {
   if (remaining_seconds_ > 0) {
     buzzer_->Beep();
 
-    return add_repeating_timer_ms(clock_clock::k1SecondDelayInMs, Clock::RepeatingTimerCallback, this, &repeating_timer_);
+    return add_repeating_timer_ms(
+      clock_clock::k1SecondDelayInMs,
+      Clock::RepeatingTimerCallback,
+      this,
+      &repeating_timer_);
   } else {
     buzzer_->Beep(2);
 
